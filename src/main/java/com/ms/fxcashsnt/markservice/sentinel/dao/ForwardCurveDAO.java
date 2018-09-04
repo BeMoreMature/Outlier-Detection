90dc4b756e24017d1879817e45faffa913e248a1
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:28:09 +0800

    Update ForwardCurveDAO.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/dao/ForwardCurveDAO.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/dao/ForwardCurveDAO.java
@@ -1,4 +1,19 @@
 package com.ms.fxcashsnt.markservice.sentinel.dao;
 
+import com.ms.fxcashsnt.markservice.sentinel.model.forward.ForwardCurve;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * user: yandong.liu
+ * date: 7/23/2018
+ */
 public interface ForwardCurveDAO {
+    ForwardCurve query(String currencyPair, String context, Instant startTimestamp, Instant endTimestamp);
+
+    List<ForwardCurve> queryForList(Instant startTimestamp, Instant endTimestamp);
+
+    List<String> queryUniqueCurrencyPair();
 }
