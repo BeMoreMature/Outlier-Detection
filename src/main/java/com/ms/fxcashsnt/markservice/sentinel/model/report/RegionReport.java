2fad3695c9af993d1a3347cb363288751e8d675b
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:38:05 +0800

    Update RegionReport.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/model/report/RegionReport.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/model/report/RegionReport.java
@@ -1,4 +1,75 @@
 package com.ms.fxcashsnt.markservice.sentinel.model.report;
 
+/**
+ * user: Carl,Wu
+ * date: 8/8/2018
+ * build format that need to insert in the table
+ */
+
 public class RegionReport {
+    private String currency;
+    private String tenor;
+    private Double NY ;
+    private Double LN ;
+    private Double TK ;
+    private Double HK ;
+
+    public void setHK(Double HK) {
+        this.HK = HK;
+    }
+
+    public void setLN(Double LN) {
+
+        this.LN = LN;
+    }
+
+    public void setTK(Double TK) {
+
+        this.TK = TK;
+    }
+
+    public void setNY(Double NY) {
+
+        this.NY = NY;
+    }
+
+    public Double getHK() {
+
+        return HK;
+    }
+
+    public Double getTK() {
+
+        return TK;
+    }
+
+    public Double getLN() {
+
+        return LN;
+    }
+
+    public Double getNY() {
+
+        return NY;
+    }
+
+    public void setTenor(String tenor) {
+
+        this.tenor = tenor;
+    }
+
+    public String getTenor() {
+
+        return tenor;
+    }
+
+    public void setCurrency(String currency) {
+
+        this.currency = currency;
+    }
+
+    public String getCurrency() {
+
+        return currency;
+    }
 }
