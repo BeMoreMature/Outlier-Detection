a6024387dc370a504888a19272a840b1c0afc4a3
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:53:38 +0800

    Update ReportBuilderTest.java

--- src/test/java/com/ms/fxcashsnt/markservice/sentinel/detector/ReportBuilderTest.java
+++ src/test/java/com/ms/fxcashsnt/markservice/sentinel/detector/ReportBuilderTest.java
@@ -1,4 +1,24 @@
 package com.ms.fxcashsnt.markservice.sentinel.detector;
 
+import com.ms.fxcashsnt.markservice.sentinel.model.report.Report;
+import com.ms.fxcashsnt.markservice.sentinel.model.report.ReportBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@ContextConfiguration(locations = {"classpath:spring-core-test.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
 public class ReportBuilderTest {
+    @Autowired
+    private CrossRegionDetector crossRegionDetector;
+    @Test
+    public void testBuilder(){
+        ReportBuilder reportBuilder = new ReportBuilder(crossRegionDetector);
+        List<Report> list = reportBuilder.build(0.2,java.time.LocalDate.now());
+
+    }
 }
