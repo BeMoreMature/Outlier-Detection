4f1cb28b6da93a4f2ad048f11070f23e3b417e4a
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:55:34 +0800

    Update RemainUnchangedStrategyTest.java

--- src/test/java/com/ms/fxcashsnt/markservice/sentinel/strategy/RemainUnchangedStrategyTest.java
+++ src/test/java/com/ms/fxcashsnt/markservice/sentinel/strategy/RemainUnchangedStrategyTest.java
@@ -1,4 +1,28 @@
 package com.ms.fxcashsnt.markservice.sentinel.strategy;
 
+import com.ms.fxcashsnt.markservice.sentinel.model.Point;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.Instant;
+import java.util.LinkedList;
+import java.util.List;
+
+@ContextConfiguration(locations = {"classpath:spring-core-test.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
 public class RemainUnchangedStrategyTest {
+
+    @Test
+    public void testRemainUnchangedStrategy(){
+            RemainUnchangedStrategy remainUnchangedStrategy = new RemainUnchangedStrategy();
+            List<Point> list = new LinkedList<>();
+            list.add(new Point(1.0, Instant.now()));
+            list.add(new Point(2.0, Instant.now()));
+            list.add(new Point(1.1, Instant.now()));
+            list.add(new Point(1.1, Instant.now()));
+            list.add(new Point(1.0, Instant.now()));
+            remainUnchangedStrategy.fit(list);
+    }
 }
