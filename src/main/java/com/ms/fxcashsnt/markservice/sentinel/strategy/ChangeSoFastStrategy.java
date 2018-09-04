7a436642355be6e87dcfe2d494d0bef83412cb36
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:42:15 +0800

    Update ChangeSoFastStrategy.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/strategy/ChangeSoFastStrategy.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/strategy/ChangeSoFastStrategy.java
@@ -1,4 +1,41 @@
 package com.ms.fxcashsnt.markservice.sentinel.strategy;
 
-public class ChangeSoFastStrategy {
+import com.ms.fxcashsnt.markservice.sentinel.model.Point;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * user: yandongl
+ * date: 7/26/2018
+ * <p>
+ * This strategy is used to find the point which changes so fast compared to the former point. The threshold is pre-defined.
+ * <p>
+ * The minimum size of pointList is 2.
+ */
+public class ChangeSoFastStrategy implements Strategy {
+    private double threashold;
+
+    public ChangeSoFastStrategy(double threashold) {
+        this.threashold = threashold;
+    }
+
+    @Override
+    public Strategy fit(List<Point> pointList) {
+        // This strategy does not need to fit the model because it has no trainable parameters.
+        return this;
+    }
+
+    @Override
+    public List<Boolean> predict(List<Point> pointList) {
+        List<Boolean> anomalyBooleanList = new LinkedList<>();
+        // The first point is always normal.
+        anomalyBooleanList.add(false);
+        for (int i = 1; i < pointList.size(); i++) {
+            anomalyBooleanList.add(Math.abs(pointList.get(i).getValue() / pointList.get(i - 1).getValue() - 1) > threashold);
+        }
+
+        return anomalyBooleanList;
+    }
+
 }
