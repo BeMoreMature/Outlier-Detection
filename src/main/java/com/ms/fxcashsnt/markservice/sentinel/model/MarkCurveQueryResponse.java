7d2489e830ac811108677983d85bb83f7f924b9b
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:41:15 +0800

    Update MarkCurveQueryResponse.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/model/MarkCurveQueryResponse.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/model/MarkCurveQueryResponse.java
@@ -1,4 +1,35 @@
 package com.ms.fxcashsnt.markservice.sentinel.model;
 
-public class MarkCurveQueryResponse {
+import msjava.hdom.Document;
+import msjava.hdom.Element;
+import msjava.hdom.Namespace;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * user: Carl, WU
+ * date: 7/7/2018
+ */
+public class MarkCurveQueryResponse implements Serializable {
+    public Map<String, MarkCurveQueryResult> markCurveQueryResultMap;
+
+    public MarkCurveQueryResponse(Document document) {
+        Element root = document.getRootElement();
+        Element ress = root.getChild("MarkCurveQueryResults", Namespace.getNamespace("http://xml.ms.com/ns/fxmessage"));
+        List<Element> queryResult = ress.getChildren("MarkCurveQueryResult", Namespace.getNamespace("http://xml.ms.com/ns/fxmessage"));     //get result list
+        // for each result, we choose currencyPair as key and new object as value
+        markCurveQueryResultMap = queryResult.stream()
+                .map(MarkCurveQueryResult::new)
+                .collect(Collectors.toMap(
+                        e -> e.getCurrencyPair(),
+                        e -> e
+                ));
+    }
+
+    public Map<String, MarkCurveQueryResult> getMarkCurveQueryResultMap() {
+        return markCurveQueryResultMap;
+    }
 }
