af3a4055bcd79ad64e8c81b06b98c050d5f07de6
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:30:30 +0800

    Update IsolationForestDetector.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/IsolationForestDetector.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/IsolationForestDetector.java
@@ -1,4 +1,19 @@
 package com.ms.fxcashsnt.markservice.sentinel.detector;
 
-public class IsolationForestDetector {
+import org.springframework.stereotype.Component;
+
+/**
+ * user: yandongl
+ * date: 8/20/2018
+ */
+@Component(value = "isolationForest")
+public class IsolationForestDetector extends AbstractPythonAbnormalityDetector {
+    public IsolationForestDetector() {
+    }
+
+    @Override
+    public void loadStrategy() {
+        super.loadStrategy("isolation_forest.py");
+    }
+
 }
