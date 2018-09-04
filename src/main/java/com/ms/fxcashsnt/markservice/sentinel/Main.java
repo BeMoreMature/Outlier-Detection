c73baf79d4767b25b5d358665e173e69ce9b9290
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:46:18 +0800

    Update Main.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/Main.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/Main.java
@@ -1,4 +1,21 @@
 package com.ms.fxcashsnt.markservice.sentinel;
 
+import com.ms.fxcashsnt.markservice.sentinel.util.MarkCurveDownloader;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import weka.classifiers.trees.J48;
+
+/**
+ * user: yandong.liu
+ * date: 7/18/2018
+ */
 public class Main {
+    public static void main(String[] args) {
+        String springConfigXML = "spring-core.xml";
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(springConfigXML);
+
+        MarkCurveDownloader markCurveDownloader = (MarkCurveDownloader) context.getBean("markCurveDownloader");
+        markCurveDownloader.downloadEODMarkCurve();
+        markCurveDownloader.work();
+    }
 }
