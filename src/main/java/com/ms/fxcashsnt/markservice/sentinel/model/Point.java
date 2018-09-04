f7eb68e4a9846a3d56f535f2864f18e9655ddcab
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:41:50 +0800

    Update Point.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/model/Point.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/model/Point.java
@@ -1,4 +1,34 @@
 package com.ms.fxcashsnt.markservice.sentinel.model;
 
-public class Point {
+import java.io.Serializable;
+import java.time.Instant;
+
+/**
+ * A general Point class used for different strategies.
+ */
+public class Point implements Serializable {
+    private double value;
+    private Instant timestamp;
+
+    public Point(){};
+    public Point(double value, Instant timestamp) {
+        this.value = value;
+        this.timestamp = timestamp;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
 }
