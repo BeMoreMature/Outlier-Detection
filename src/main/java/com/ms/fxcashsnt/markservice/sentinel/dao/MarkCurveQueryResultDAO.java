7ce65d7ce2ae93a99b26add719f3a31b325b46b7
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:28:24 +0800

    Update MarkCurveQueryResultDAO.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/dao/MarkCurveQueryResultDAO.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/dao/MarkCurveQueryResultDAO.java
@@ -1,4 +1,16 @@
 package com.ms.fxcashsnt.markservice.sentinel.dao;
 
+import com.ms.fxcashsnt.markservice.sentinel.model.MarkCurveQueryResult;
+
+import java.util.List;
+
+/**
+ * user: yandong.liu
+ * date: 7/17/2018
+ */
 public interface MarkCurveQueryResultDAO {
+    void batchSave(List<MarkCurveQueryResult> resultList);
+
+    void printAllRecord();
+
 }
