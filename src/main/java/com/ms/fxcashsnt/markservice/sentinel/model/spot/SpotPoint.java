d8f24f7bf55d2b4634d231d4f53bf4182cb6a759
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:40:18 +0800

    Update SpotPoint.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/model/spot/SpotPoint.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/model/spot/SpotPoint.java
@@ -1,4 +1,58 @@
 package com.ms.fxcashsnt.markservice.sentinel.model.spot;
 
+import java.time.Instant;
+import java.time.LocalDate;
+
+/**
+ * user: yandong.liu
+ * date: 7/23/2018
+ */
 public class SpotPoint {
+    private double spotRate;
+    private LocalDate spotDate;
+    private LocalDate positionDate;
+
+    private Instant timestamp;
+
+    public SpotPoint() {
+    }
+
+    public SpotPoint(double spotRate, LocalDate spotDate, LocalDate positionDate, Instant timestamp) {
+        this.spotRate = spotRate;
+        this.spotDate = spotDate;
+        this.positionDate = positionDate;
+        this.timestamp = timestamp;
+    }
+
+    public double getSpotRate() {
+        return spotRate;
+    }
+
+    public void setSpotRate(double spotRate) {
+        this.spotRate = spotRate;
+    }
+
+    public LocalDate getSpotDate() {
+        return spotDate;
+    }
+
+    public void setSpotDate(LocalDate spotDate) {
+        this.spotDate = spotDate;
+    }
+
+    public LocalDate getPositionDate() {
+        return positionDate;
+    }
+
+    public void setPositionDate(LocalDate positionDate) {
+        this.positionDate = positionDate;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
 }
