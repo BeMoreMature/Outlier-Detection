a10e2519cf99994caa62efd485ab4d20ef2f8ff1
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:30:43 +0800

    Update LocalOutlierFactorDetector.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/LocalOutlierFactorDetector.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/LocalOutlierFactorDetector.java
@@ -1,4 +1,18 @@
 package com.ms.fxcashsnt.markservice.sentinel.detector;
 
-public class LocalOutlierFactorDetector {
+import org.springframework.stereotype.Component;
+
+/**
+ * user: yandongl
+ * date: 8/20/2018
+ */
+@Component(value = "localOutlierFactorDetector")
+public class LocalOutlierFactorDetector extends AbstractPythonAbnormalityDetector {
+    public LocalOutlierFactorDetector() {
+    }
+
+    @Override
+    public void loadStrategy() {
+        super.loadStrategy("local_outlier_factor.py");
+    }
 }
