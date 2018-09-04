2e9568cfd0bfd6a990d3607d35c248814652da48
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:29:41 +0800

    Update ChangeSoFastDetector.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/ChangeSoFastDetector.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/ChangeSoFastDetector.java
@@ -1,4 +1,39 @@
 package com.ms.fxcashsnt.markservice.sentinel.detector;
 
-public class ChangeSoFastDetector {
+import com.ms.fxcashsnt.markservice.sentinel.strategy.ChangeSoFastStrategy;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * user: yandongl
+ * date: 7/31/2018
+ */
+@Component(value = "spotChangeSoFastDetector")
+public class ChangeSoFastDetector extends AbstractAbnormalityDetector {
+    private double threashold;
+
+    public ChangeSoFastDetector() {
+        super();
+    }
+
+    @Override
+    public void loadStrategy() {
+        super.setStrategy(new ChangeSoFastStrategy(threashold));
+    }
+
+    @Override
+    public boolean isNeedReport(List<Boolean> anomalyBooleanList) {
+        return anomalyBooleanList.stream()
+                .anyMatch(Boolean::booleanValue);
+    }
+
+    public double getThreashold() {
+        return threashold;
+    }
+
+    public void setThreashold(double threashold) {
+        this.threashold = threashold;
+        this.loadStrategy();
+    }
 }
