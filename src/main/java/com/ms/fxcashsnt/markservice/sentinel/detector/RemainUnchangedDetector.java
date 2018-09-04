e15eaef61cb47540cb915fef1be42cf6bb986751
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:31:31 +0800

    Update RemainUnchangedDetector.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/RemainUnchangedDetector.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/RemainUnchangedDetector.java
@@ -1,4 +1,40 @@
 package com.ms.fxcashsnt.markservice.sentinel.detector;
 
-public class RemainUnchangedDetector {
+import com.ms.fxcashsnt.markservice.sentinel.strategy.RemainUnchangedStrategy;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * user: yandongl
+ * date: 7/31/2018
+ */
+@Component(value = "remainUnchangedDetector")
+public class RemainUnchangedDetector extends AbstractAbnormalityDetector {
+    private double threshold;
+
+    public RemainUnchangedDetector() {
+        super();
+    }
+
+    @Override
+    public void loadStrategy() {
+        super.setStrategy(new RemainUnchangedStrategy());
+    }
+
+    @Override
+    boolean isNeedReport(List<Boolean> anomalyBooleanList) {
+        if (anomalyBooleanList.size() < 2 ) return false;
+        return anomalyBooleanList.stream()
+                .filter(Boolean::booleanValue)
+                .count() / anomalyBooleanList.size() > threshold;
+    }
+
+    public double getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(double threshold) {
+        this.threshold = threshold;
+    }
 }
