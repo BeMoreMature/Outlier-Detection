7e6f518705db93cb2924df068d2f42ec3947bcdb
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:26:38 +0800

    Update IndexController.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/controller/view/IndexController.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/controller/view/IndexController.java
@@ -1,4 +1,19 @@
 package com.ms.fxcashsnt.markservice.sentinel.controller.view;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * user: yandongl
+ * date: 8/28/2018
+ */
+@Controller
 public class IndexController {
+
+    @RequestMapping(value = "/sentinel")
+    public String index() {
+        return "index.html";
+    }
 }
