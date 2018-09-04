15353f4b0792b05bdec7b5815b4c3018c5281a45
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:44:54 +0800

    Update MarkServiceConstants.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/util/MarkServiceConstants.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/util/MarkServiceConstants.java
@@ -1,4 +1,16 @@
 package com.ms.fxcashsnt.markservice.sentinel.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * user: yandong.liu
+ * date: 7/16/2018
+ */
 public interface MarkServiceConstants {
+    List<String> IntraContextList = Arrays.asList("ASIA", "TKEOD", "LNEOD", "NYEOD");
+    List<String> EndContextList = Arrays.asList("HKFRM", "TKFRM", "LNFRM", "NYFRM");
+    List<String> IgnoreCurrencyList = Arrays.asList("ARA", "ARP", "ATS", "BEF", "BRC", "BRR", "CSD", "CYP", "DEM", "ECS", "EEK", "ESP", "FIM", "FRF", "GHC", "GRD", "IEP", "ITL", "LUF", "LVL", "MTL", "NLG", "PLZ", "PTE", "ROL", "RUR", "TRL", "VEB", "XEU", "ZAL");
 }
+
