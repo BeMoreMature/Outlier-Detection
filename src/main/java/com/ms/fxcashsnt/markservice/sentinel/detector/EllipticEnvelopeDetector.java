2e0f6ba75494543ac52074a68b0603095b6ff19c
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:30:16 +0800

    Update EllipticEnvelopeDetector.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/EllipticEnvelopeDetector.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/EllipticEnvelopeDetector.java
@@ -1,4 +1,21 @@
 package com.ms.fxcashsnt.markservice.sentinel.detector;
 
-public class EllipticEnvelopeDetector {
+import org.springframework.stereotype.Component;
+
+/**
+ * user: yandongl
+ * date: 8/15/2018
+ */
+@Component(value = "ellipticEnvelopeDetector")
+public class EllipticEnvelopeDetector extends AbstractPythonAbnormalityDetector {
+
+    public EllipticEnvelopeDetector() {
+    }
+
+    @Override
+    public void loadStrategy() {
+        super.loadStrategy("elliptic_envelope.py");
+    }
+
+
 }
