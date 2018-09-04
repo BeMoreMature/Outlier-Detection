02bffd56acc5fd59f07e45b3e06ab7675240209a
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:43:29 +0800

    Update Strategy.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/strategy/Strategy.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/strategy/Strategy.java
@@ -1,4 +1,10 @@
 package com.ms.fxcashsnt.markservice.sentinel.strategy;
 
+import com.ms.fxcashsnt.markservice.sentinel.model.Point;
+
+import java.util.List;
+
 public interface Strategy {
+    Strategy fit(List<Point> pointList);
+    List<Boolean> predict(List<Point> pointList);
 }
