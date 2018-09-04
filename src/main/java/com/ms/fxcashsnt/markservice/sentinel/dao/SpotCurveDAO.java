b7d8a55b15ab6c4455fef09784150de173d8ec58
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:28:41 +0800

    Update SpotCurveDAO.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/dao/SpotCurveDAO.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/dao/SpotCurveDAO.java
@@ -1,4 +1,19 @@
 package com.ms.fxcashsnt.markservice.sentinel.dao;
 
+import com.ms.fxcashsnt.markservice.sentinel.model.spot.SpotCurve;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * user: yandong.liu
+ * date: 7/23/2018
+ */
 public interface SpotCurveDAO {
+    SpotCurve query(String currencyPair, String context, Instant startTimestamp, Instant endTimestamp);
+
+    List<SpotCurve> queryForList(Instant startTimestamp, Instant endTimestamp);
+
+    List<String> queryUniqueCurrencyPair();
 }
