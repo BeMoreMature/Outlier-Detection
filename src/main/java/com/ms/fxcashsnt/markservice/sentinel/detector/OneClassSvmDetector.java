54123bc7a9d38667ef37778cec97aa47c4fe3c23
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:30:56 +0800

    Update OneClassSvmDetector.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/OneClassSvmDetector.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/detector/OneClassSvmDetector.java
@@ -1,4 +1,19 @@
 package com.ms.fxcashsnt.markservice.sentinel.detector;
 
-public class OneClassSvmDetector {
+import org.springframework.stereotype.Component;
+
+/**
+ * user: yandongl
+ * date: 8/20/2018
+ */
+@Component(value = "oneClassSvmDetector")
+public class OneClassSvmDetector extends AbstractPythonAbnormalityDetector {
+    public OneClassSvmDetector() {
+    }
+
+    @Override
+    public void loadStrategy() {
+        super.loadStrategy("one_class_svm.py");
+    }
+
 }
