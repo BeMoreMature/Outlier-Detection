7f69395a6a0701294dcb6465ba11cede51229efc
BeMoreMature <38548099+BeMoreMature@users.noreply.github.com>
2018-09-04 10:44:24 +0800

    Update DetectorConstants.java

--- src/main/java/com/ms/fxcashsnt/markservice/sentinel/util/DetectorConstants.java
+++ src/main/java/com/ms/fxcashsnt/markservice/sentinel/util/DetectorConstants.java
@@ -1,4 +1,16 @@
 package com.ms.fxcashsnt.markservice.sentinel.util;
 
+/**
+ * user: yandongl
+ * date: 8/7/2018
+ */
 public interface DetectorConstants {
+    String CHANGE_SO_FAST = "CHANGE_SO_FAST";
+    String REMAIN_UNCHANGED = "REMAIN_UNCHANGED";
+    String CROSS_REGION = "CROSS_REGION";
+    String ELLIPTIC_ENVELOPE = "ELLIPTIC_ENVELOP";
+    String ISOLATION_FOREST = "ISOLATION_FOREST";
+    String ONE_CLASS_SVM = "ONE_CLASS_SVM";
+    String LOCAL_OUTLIER_FACTOR = "LOCAL_OUTLIER_FACTOR";
+    String SMOOTHED_Z_SCORE = "SMOOTHED_Z_SCORE";
 }
